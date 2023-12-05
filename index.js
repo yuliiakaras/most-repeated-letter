b7a9f927442e07b58b08983a83a9d14db0daf2e7
yuliia_karas <yuliiakaras03@gmail.com>
2023-12-05 21:53:00 +0300

    fix code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 function findMostRepeatedLetter(filePath) {
     const text = fs.readFileSync(filePath, 'utf-8');
 
@@ -22,13 +21,6 @@ function findMostRepeatedLetter(filePath) {
     });
 
     return mostRepeatedLetter;
-}
-
-if (process.argv.length > 2) {
-    const filePath = process.argv[2];
-    console.log('Most repeated letter in', filePath, 'is', findMostRepeatedLetter(filePath));
-} else {
-    console.log('Please provide a file path. Example usage: node index.js text.txt');
 }
 
 module.exports = findMostRepeatedLetter;
